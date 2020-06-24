b90893b35879e3cf9780131c29fdc94e36518688
shengli-sube <feiaotianxia@163.com>
2020-06-24 12:20:36 +0800

    第三次作业提交 卞凌风19050300032

---
+++ lesson3/amazing_elzing1.c
@@ -0,0 +1,56 @@
+char ch=' ';
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+  Serial.println("ok!");
+}
+
+/*
+预定
+'f':表示前进
+'b':表示后退
+'r':表示右转
+'l':表示左转
+'s':表示停止
+*/
+void loop()
+{
+  if(Serial.available()>0)
+  {
+  	ch=Serial.read();
+    if(ch=='f'){
+      digitalWrite(6,HIGH);
+      digitalWrite(7,LOW);
+      digitalWrite(8,HIGH);
+      digitalWrite(9,LOW);
+    }
+    else if(ch=='b'){
+      digitalWrite(7,HIGH);
+      digitalWrite(6,LOW);
+      digitalWrite(9,HIGH);
+      digitalWrite(8,LOW);
+    }
+    else if(ch=='r'){
+      digitalWrite(7,HIGH);
+      digitalWrite(6,LOW);
+      digitalWrite(8,HIGH);
+      digitalWrite(9,LOW);
+    }
+    else if(ch=='l'){
+      digitalWrite(6,HIGH);
+      digitalWrite(7,LOW);
+      digitalWrite(9,HIGH);
+      digitalWrite(8,LOW);
+    }
+    else if(ch=='s'){
+      digitalWrite(6,LOW);
+      digitalWrite(7,LOW);
+      digitalWrite(8,LOW);
+      digitalWrite(9,LOW);
+    }
+  }
+}
